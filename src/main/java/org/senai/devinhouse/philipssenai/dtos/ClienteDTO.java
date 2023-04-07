f37fa0b2ba0fc503c3d36aca5835927851bb53ff
GitdoPedro <emailpedrolima@gmail.com>
2023-04-07 08:29:18 -0300

    [M2S09] Ex 8 - Projeto Market - Criando a Classe Cliente

---
+++ src/main/java/org/senai/devinhouse/philipssenai/dtos/ClienteDTO.java
@@ -0,0 +1,42 @@
+package org.senai.devinhouse.philipssenai.dtos;
+public class ClienteDTO {
+
+    private int id;
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    private String CPF;
+    private String dataNascimento;
+
+}
---
+++ src/main/java/org/senai/devinhouse/philipssenai/models/Cliente.java
@@ -0,0 +1,57 @@
+package org.senai.devinhouse.philipssenai.models;
+
+import jakarta.persistence.*;
+
+@Entity(name = "clientes")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String CPF;
+    private String dataNascimento;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @ManyToMany
+    private Produto produto;
+}
